56182f829d2da47b04071eb2c72aff00f507cae5
Guilherme Alves Profirio <guilherme.alves@devvo.com.br>
2025-02-18 15:57:59 -0300

    fix: any type

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,6 +1,41 @@
 import { abacatePay, redis, supabase } from "@/services/clients";
 import { NextResponse } from "next/server";
 
+interface Product {
+  id: string;
+  externalId: string;
+  quantity: number;
+}
+
+interface CustomerMetadata {
+  name: string;
+  cellphone: string;
+  taxId: string;
+  email: string;
+}
+
+interface Customer {
+  id: string;
+  metadata: CustomerMetadata;
+}
+
+interface Metadata {
+  fee: number;
+  returnUrl: string;
+  completionUrl: string;
+}
+
+interface Bill {
+  id: string;
+  frequency: "ONE_TIME" | "RECURRING";
+  url: string;
+  amount: number;
+  status: "PAID" | "PENDING" | "CANCELLED";
+  devMode: boolean;
+  methods: string[];
+  products: Product[];
+}
+
 export async function POST(req: Request){
   const {
     firstName,
@@ -18,7 +53,7 @@ export async function POST(req: Request){
 
   console.log(payments.data.data);
 
-  const payment = payments.data.data.find((payment: any) => payment.id === paymentId);
+  const payment = payments.data.data.find((payment: Bill) => payment.id === paymentId);
 
   if (!payment) {
     redis.del(slug);
